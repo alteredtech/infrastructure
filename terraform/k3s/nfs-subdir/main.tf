f38b51a1c96ebe80f0d1e72342e1a714d5400b2b
Michael Einreinhof <michael@einreinhof.com>
2022-03-02 15:27:49 -0700

    feat(terraform): added nfs-subdir for k3s

---
+++ terraform/k3s/nfs-subdir/main.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "nfs_subdir" {
+  name       = "nfs-subdir"
+  repository = "https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner"
+  chart = "nfs-subdir-external-provisioner"
+    version = "4.0.16"
+
+    values = [
+    "${file("values-nfs-subdir.yml")}"
+  ]
+
+}
