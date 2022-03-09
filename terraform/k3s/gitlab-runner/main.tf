63587a6072bfd761b6613c5bc332a79e4613fb13
Michael Einreinhof <michael@einreinhof.com>
2022-03-09 10:02:49 -0700

    feat(terraform): gitlab runner

---
+++ terraform/k3s/gitlab-runner/main.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "prometheus-monitoring" {
+  name       = "prom-monitor"
+  create_namespace = true
+  namespace = "monitor-prom"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart = "kube-prometheus-stack"
+    version = "33.2.0"
+
+    values = [
+    "${file("values-test.yml")}"
+  ]
+}
